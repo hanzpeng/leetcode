258ec1aac7c0c46e57e0bd665d8b728476b8bfd6
Hanz Zhang <hanzzhan@microsoft.com>
2021-05-09 15:12:49 -0700

    initial checkin

---
+++ P025_ReverseLinkedList.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    class P025_ReverseLinkedList
+    {
+        
+        [Test]
+        public void test1()
+        {
+            Node n1 = new Node(1);
+            Node n2 = new Node(2);
+            Node n3 = new Node(3);
+            Node n4 = new Node(4);
+            Node n5 = new Node(5);
+            Node n6 = new Node(6);
+            Node n7 = new Node(7);
+            Node n8 = new Node(8);
+
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4;
+            n4.next = n5;
+            n5.next = n6;
+            n6.next = n7;
+            n7.next = n8;
+
+            Node cur = ReverseKGroup(n1,3);
+            while(cur != null)
+            {
+                Console.WriteLine(cur.val);
+                cur = cur.next;
+            }
+
+            Assert.AreEqual(4, 4);
+        }
+
+        public Node ReverseKGroup(Node head, int k)
+        {
+            if (head == null || head.next == null) return head;
+
+            var res = ReverseK(head,  k);
+            head = res[0];
+            Node tail = res[1];
+            Node nextHead = res[2];
+            while(nextHead != null)
+            {
+                res = ReverseK(nextHead, k);
+                tail.next = res[0];
+                tail = res[1];
+                nextHead = res[2];
+            }
+
+            return head;
+        }
+
+        public Node[] ReverseK(Node head, int k)
+        {
+            if (head == null || head.next == null) return new Node[] { head, null, null};
+            Node pre = head;
+            Node cur = head.next;
+            int count = 1;
+            while (cur != null && count < k)
+            {
+                count++;
+                Node temp = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = temp;
+            }
+            head.next = null;
+            return new Node[] { pre, head, cur}; // head, tail, nextHead
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public Node Reverse(Node head, Node nextHead, int k)
+        {
+            if (head == null || head.next == null) return head;
+            Node pre = head;
+            Node cur = head.next;
+            int count = 1;
+            while(cur != null && count < k)
+            {
+                count++;
+                Node temp = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = temp;
+            }
+            head.next = nextHead;
+            return pre;
+        }
+
+        public class Node
+        {
+            public int val;
+            public Node next;
+            public Node(int x) { val = x; }
+        }
+    }
+}
---
+++ P1308-ez.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System;
+
+namespace LeetCode
+{
+    class P1308_ez
+    {
+        [Test]
+        public void test1()
+        {
+            int[][] m = { new[]{ 1000, 1001 },new[]{ 1010, 1011 } };
+            Assert.AreEqual(1000, m[0][0]);
+            Assert.AreEqual(1001, m[0][1]);
+            Assert.AreEqual(1010, m[1][0]);
+            Assert.AreEqual(1011, m[1][1]);
+
+            int[,] mm = { { 1000, 1001 }, { 1010, 1011 } };
+            Assert.AreEqual(1000, mm[0,0]);
+            Assert.AreEqual(1001, mm[0,1]);
+            Assert.AreEqual(1010, mm[1,0]);
+            Assert.AreEqual(1011, mm[1,1]);
+
+            int[][] arr = { new[] { 1, 2 }, new[] { 3, 4 } };
+        }
+
+        public int[] GetMagicNumbers(int[][] matrix)
+        {
+            return new int[] { 1, 2 };
+        }
+    }
+
+}
+
