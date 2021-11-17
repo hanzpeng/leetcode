01c0f4c27d867132b52814311c090eb780396570
Hanz Zhang <hanzzhan@microsoft.com>
2021-11-16 23:23:06 -0800

    update

--- P2065.cs
+++ P2065.cs
@@ -1,12 +1,36 @@
 using NUnit.Framework;
-namespace Leetcode 
+namespace Leetcode
 {
     public class P2065
     {
         [Test]
         public void Test1()
         {
-            Assert.AreEqual(1,1);          
+            var values = new int[] {1,2,3};
+            var edges = new int[][]
+            {
+                new int[] {100,101,102},
+                new int[] {110,111,112},
+                new int[] {120,121,122},
+            };
+            Assert.AreEqual(101, edges[0][1]);
+            Assert.AreEqual(120, edges[2][0]);
+
+            var twoD = new int[,] 
+            {
+                {100,101,102},
+                {110,111,112},
+                {120,121,122}
+            };
+            Assert.AreEqual(101, twoD[0,1]);
+            Assert.AreEqual(120, twoD[2,1]);
+
+            Assert.AreEqual(1, 1);
+        }
+        public int MaximalPathQuality(int[] values, int[][] edges, int maxTime)
+        {
+            return 1;
+
         }
     }
 }
