961a04e138dca52109d097a7e7a3faf1de82c2d0
Hanz Zhang <hanzzhan@microsoft.com>
2021-11-16 23:04:44 -0800

    update

---
+++ P2065.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+namespace Leetcode 
+{
+    public class P2065
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1,1);          
+        }
+    }
+}
