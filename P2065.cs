bef3167750afe5da1cf02e94416657225af927e1
Hanz Zhang <hanzzhan@microsoft.com>
2021-11-18 15:32:59 -0800

    update

--- P2065.cs
+++ P2065.cs
@@ -10,9 +10,9 @@ namespace Leetcode
         {
             Assert.AreEqual(1, 1);
             Assert.AreEqual(75, new Solution().MaximalPathQuality(new int[] { 0, 32, 10, 43 }, new int[][] { new int[] { 0, 1, 10 }, new int[] { 1, 2, 15 }, new int[] { 0, 3, 10 } }, 49));
-            Assert.AreEqual(25, new Solution().MaximalPathQuality(new int[] {5,10,15,20 }, new int[][] { new int[] { 0, 1, 10 }, new int[] { 1, 2, 10 }, new int[] { 0, 3, 10 } }, 30));
-            Assert.AreEqual(7, new Solution().MaximalPathQuality(new int[] {1,2,3,4 }, new int[][] { new int[] { 0, 1, 10 }, new int[] { 1, 2, 11 }, new int[] { 2, 3, 12 },new int[] { 1, 3, 13 } }, 50));
-            Assert.AreEqual(0, new Solution().MaximalPathQuality(new int[] {0,1,2 }, new int[][] { new int[] { 0, 2, 10 }}, 10));
+            Assert.AreEqual(25, new Solution().MaximalPathQuality(new int[] { 5, 10, 15, 20 }, new int[][] { new int[] { 0, 1, 10 }, new int[] { 1, 2, 10 }, new int[] { 0, 3, 10 } }, 30));
+            Assert.AreEqual(7, new Solution().MaximalPathQuality(new int[] { 1, 2, 3, 4 }, new int[][] { new int[] { 0, 1, 10 }, new int[] { 1, 2, 11 }, new int[] { 2, 3, 12 }, new int[] { 1, 3, 13 } }, 50));
+            Assert.AreEqual(0, new Solution().MaximalPathQuality(new int[] { 0, 1, 2 }, new int[][] { new int[] { 0, 2, 10 } }, 10));
         }
         public class Solution
         {
@@ -21,14 +21,16 @@ namespace Leetcode
             Dictionary<int, List<int>> g = new Dictionary<int, List<int>>();
             Dictionary<string, int> t = new Dictionary<string, int>();
             int maxQuality = -1;
+            int[] hitCount;
             public int MaximalPathQuality(int[] values, int[][] edges, int maxTime)
             {
                 this.edges = edges;
                 this.values = values;
-                bool[] visited = new bool[values.Length];
+                hitCount = new int[values.Length];
                 BuildGraph();
                 BuildTimes();
-                Dfs(0, 0, maxTime, new HashSet<int>());
+                hitCount[0] = 1;
+                Dfs(0, 0, maxTime);
                 return this.maxQuality;
             }
             void BuildGraph()
@@ -56,18 +58,17 @@ namespace Leetcode
             }
             string Key(int u, int v)
             {
-                return Math.Min(u, v) + "," + Math.Max(u,v);
+                return Math.Min(u, v) + "," + Math.Max(u, v);
             }
-            void Dfs(int node, int quality, int time, HashSet<int> parents)
+            void Dfs(int node, int quality, int time)
             {
                 if (time < 0)
                 {
                     return;
                 }
 
-                if (!parents.Contains(node))
+                if (hitCount[node] == 1)
                 {
-                    parents.Add(node);
                     quality += values[node];
                 }
 
@@ -78,7 +79,9 @@ namespace Leetcode
 
                 foreach (int next in g[node])
                 {
-                    Dfs(next, quality, time - t[Key(node, next)], new HashSet<int>(parents));
+                    hitCount[next]++;
+                    Dfs(next, quality, time - t[Key(node, next)]);
+                    hitCount[next]--; // backtracking
                 }
             }
         }
