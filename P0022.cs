43813aa685486cdc1afd100af0378e99da4357d1
Hanz Zhang <hanzzhan@microsoft.com>
2021-11-19 00:13:46 -0800

    update P0022

--- P0022.cs
+++ P0022.cs
@@ -1,12 +1,43 @@
 using NUnit.Framework;
-namespace Leetcode 
+using System.Collections.Generic;
+
+namespace Leetcode
 {
     public class P0022
     {
         [Test]
         public void Test1()
         {
-            Assert.AreEqual(1,1);          
+            Assert.AreEqual(1, 1);
+        }
+        public IList<string> GenerateParenthesis(int n)
+        {
+            List<string> result = new List<string>();
+            backtrack(result, n, 0, 0, "");
+            return result;
+        }
+
+        void backtrack(List<string> result, int n, int left, int right, string str)
+        {
+            if (left == n)
+            {
+                string newStr = str;
+                for (int i = 1; i <= n - right; i++)
+                {
+                    str += ")";
+                }
+                result.Add(str);
+                return;
+            }
+            if (left == right)
+            {
+                backtrack(result, n, left + 1, right, str + "(");
+            }
+            if (left > right)
+            {
+                backtrack(result, n, left + 1, right, str + "(");
+                backtrack(result, n, left, right + 1, str + ")");
+            }
         }
     }
 }
