c8dcd3022d64125e0dc0fd61440b29488793c284
Hanz Zhang <hanzzhan@microsoft.com>
2021-11-18 22:51:51 -0800

    P0022

--- P0022.cs
+++ P0022.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
 using NUnit.Framework;
 namespace Leetcode 
 {
-    public class P0000
+    public class P0022
     {
         [Test]
-        public void Test()
+        public void Test1()
         {
             Assert.AreEqual(1,1);          
         }
